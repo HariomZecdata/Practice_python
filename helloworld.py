b3f55e599cf486e41d1229c181de77dea476f217
HP <abhinay.s@zecdata.com>
2024-05-08 16:15:45 +0530

    files

---
+++ helloworld.py
@@ -0,0 +1,9 @@
+print('hello world')
+li= ["apple","banana","mango","orange"]
+for x in li:
+    print(x)
+print(type(li)) #check whhich type of variable
+if "apple5" in li:
+    print("true")
+else :
+  print("false")
