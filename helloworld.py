07613d03a63f46eb9b94dd4f85444ddda19a28e6
Hariom <hariom.a@zecdata.com>
2024-05-08 16:34:14 +0530

    check variable type

--- helloworld.py
+++ helloworld.py
@@ -1,9 +1,19 @@
 print('hello world')
+
 li= ["apple","banana","mango","orange"]
 for x in li:
     print(x)
 print(type(li)) #check whhich type of variable
-if "apple5" in li:
+
+
+if "apple5" in li: # if-else condition
     print("true")
 else :
   print("false")
+
+y="Hariom Asati"
+print(type(y))
+
+# https://github.com/HariomZecdata/Practice_python.git
+
+
